cc54d5e5ca02d34c090ffc5570553e0be0b1f05c
RuShakya <shakyarustina012@gmail.com>
2023-08-12 22:23:54 +0545

    Update

--- Quiz.py
+++ Quiz.py
@@ -4,21 +4,48 @@ print("Welcome to my Computer quiz")
 
 playing = input ("Do you want to play ??")
 
-if playing != "yes":
-    quit() 
+if playing.lower() != "yes":
+    quit()                  # quits the program
 
 print("Okay! Let's Playyy")
+score = 0                   # to track the score of the correct answers 
+
 
 answer = input(" What does CPU stands for? ")
-if answer == "Central Processing Unit":
-    print("Correct!")
+if answer.lower() == "central processing unit":             # .lower() to make the program read in lower case (program will automatically convert it into lower case)
+    print("Correct!")                                       # but the one written in double code "...." must be in lower case
+    score += 1              # score increment 
 else:
     print("Incorrect!")
 
 
 answer = input(" What does RAM stands for? ")
-if answer == "Random Access Memory":
+if answer.lower() == "random access memory":
     print("Correct!")
+    score += 1              # score increment 
 else:
     print("Incorrect!")
 
+
+answer = input(" What does ROM stands for? ")
+if answer.lower() == "read only memory":
+    print("Correct!")
+    score += 1              # score increment 
+else:
+    print("Incorrect!")
+
+
+answer = input(" What does GPU stands for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1              # score increment 
+else:
+    print("Incorrect!")
+
+
+print ("You got " +  str(score)  + " questions correct!")         # score stored in string to join in that sentence
+print ("You got " + str((score/4)*100) +    "%")                  # for percentage
+
+
+
+# end of first mini project

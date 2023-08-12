03e8da3900ee7261574fa930d19bd88b7b29e3ef
RuShakya <shakyarustina012@gmail.com>
2023-08-12 18:23:16 +0545

    Quiz Game

---
+++ Quiz.py
@@ -0,0 +1,17 @@
+print("Welcome to my Computer quiz")
+
+# Making a Quiz Game
+
+playing = input ("Do you want to play ??")
+
+if playing != "yes":
+    quit() 
+
+print("Okay! Let's Playyy")
+
+answer = input(" What does CPU stands for? ")
+if answer == "Central Processing Unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
